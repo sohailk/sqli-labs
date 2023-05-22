5bd05746b95c7ec07fa9c0dd50163cd5d294db7a
Sohail Khan <sohail.khan67@gmail.com>
2023-05-22 13:07:31 +0300

    Bugfix sql-lab.sql

--- sql-lab.sql
+++ sql-lab.sql
@@ -31,7 +31,7 @@ CREATE TABLE referers
 		PRIMARY KEY (id)
 		);
 
-INSERT INTO security.users (id, username, password) VALUES ('1', 'josmi', '6a019887ee578f2896fac2764792508705739338', 'https://twitter.com/josmi3123'), ('2', 'Angelina', '911ddc3b8f9a13b5499b6bc4638a2b4f3f68bf23', '-'), ('3', 'Dummy', '1ff2b3704aede04eecb51e50ca698efd50a1379b', ''), ('4', 'secure', '109f4b3c50d7b0df729d299bc6f8e9ef9066971f', ''), ('5', 'stupid', 'b444ac06613fc8d63795be9ad0beaf55011936ac', ''), ('6', 'superman', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', '');
+INSERT INTO security.users (id, username, password, about) VALUES ('1', 'josmi', '6a019887ee578f2896fac2764792508705739338', 'https://twitter.com/josmi3123'), ('2', 'Angelina', '911ddc3b8f9a13b5499b6bc4638a2b4f3f68bf23', '-'), ('3', 'Dummy', '1ff2b3704aede04eecb51e50ca698efd50a1379b', ''), ('4', 'secure', '109f4b3c50d7b0df729d299bc6f8e9ef9066971f', ''), ('5', 'stupid', 'b444ac06613fc8d63795be9ad0beaf55011936ac', ''), ('6', 'superman', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', '');
 
 INSERT INTO `security`.`emails` (id, email_id) VALUES ('1', 'Dumb@dhakkan.com'), ('2', 'Angel@iloveu.com'), ('3', 'Dummy@dhakkan.local'), ('4', 'secure@dhakkan.local'), ('5', 'stupid@dhakkan.local'), ('6', 'superman@dhakkan.local'), ('7', 'batman@dhakkan.local'), ('8', 'admin@dhakkan.com');
 
